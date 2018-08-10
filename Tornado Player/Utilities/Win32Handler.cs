9fc5304d54bdd0d2c50a28e4b59c3474e51b9bb0
Aleksbgbg <kozichka01@abv.bg>
2018-08-10 17:27:48 +0100

    [Win32Handler] Create static class to manage Win32 items, such as a handle to the main window. Initialisation logic included, so users can wait until initialisation has finished.

---
+++ Tornado Player/Utilities/Win32Handler.cs
@@ -0,0 +1,24 @@
+﻿namespace Tornado.Player.Utilities
+{
+    using System;
+    using System.Windows;
+    using System.Windows.Interop;
+
+    internal static class Win32Handler
+    {
+        internal static event EventHandler Initialised;
+
+        internal static bool IsInitialised { get; private set; }
+
+        // Guarantee this is set after MainWindow creation
+        internal static IntPtr MainWindowHandle { get; private set; }
+
+        internal static void Initialise(Window mainWindow)
+        {
+            MainWindowHandle = new WindowInteropHelper(mainWindow).Handle;
+
+            IsInitialised = true;
+            Initialised?.Invoke(null, EventArgs.Empty);
+        }
+    }
+}
