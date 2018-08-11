9948b84bb7818a60858032078f75ced91a62b17c
Aleksbgbg <kozichka01@abv.bg>
2018-08-11 20:56:10 +0100

    [TornadoPlayer] Implement class to wrap around the standard MediaPlayer class.

---
+++ Tornado Player/Utilities/TornadoPlayer.cs
@@ -0,0 +1,86 @@
+﻿namespace Tornado.Player.Utilities
+{
+    using System;
+    using System.Windows.Media;
+
+    using Tornado.Player.Models;
+
+    internal class TornadoPlayer
+    {
+        private readonly MediaPlayer _mediaPlayer = new MediaPlayer();
+
+        private bool _isPlaying;
+
+        private Track[] _tracks;
+
+        internal TornadoPlayer()
+        {
+            _mediaPlayer.MediaEnded += (sender, e) => Next();
+        }
+
+        private int _trackIndex;
+        private int TrackIndex
+        {
+            get => _trackIndex;
+
+            set
+            {
+                if (value < 0)
+                {
+                    throw new InvalidOperationException("Cannot play a track at index smaller than 0.");
+                }
+
+                if (value > _tracks.Length)
+                {
+                    _trackIndex = value % _tracks.Length;
+                }
+                else
+                {
+                    _trackIndex = value;
+                }
+            }
+        }
+
+        internal void Load(Track[] tracks)
+        {
+            _tracks = tracks;
+            Switch(0);
+        }
+
+        internal void Next()
+        {
+            Switch(TrackIndex + 1);
+        }
+
+        internal void Play()
+        {
+            _mediaPlayer.Play();
+            _isPlaying = true;
+        }
+
+        internal void Pause()
+        {
+            _mediaPlayer.Pause();
+            _isPlaying = false;
+        }
+
+        internal void TogglePlay()
+        {
+            if (_isPlaying)
+            {
+                Pause();
+            }
+            else
+            {
+                Play();
+            }
+        }
+
+        private void Switch(int index)
+        {
+            TrackIndex = index;
+            _mediaPlayer.Open(new Uri(_tracks[TrackIndex].Filepath));
+            Play();
+        }
+    }
+}
