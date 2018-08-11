c6fb9d09ed0112886c157369f1780d2a1704a239
Aleksbgbg <kozichka01@abv.bg>
2018-08-11 10:30:20 +0100

    [Track] Add model for storing information about individual tracks.

---
+++ Tornado Player/Models/Track.cs
@@ -0,0 +1,16 @@
+﻿namespace Tornado.Player.Models
+{
+    using System.IO;
+
+    internal class Track
+    {
+        internal Track(string filepath)
+        {
+            Filepath = filepath;
+        }
+
+        public string Filepath { get; }
+
+        public string Name => Path.GetFileNameWithoutExtension(Filepath);
+    }
+}
