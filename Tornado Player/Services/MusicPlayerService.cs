dec41c563ec01d8c523a027d4bd0b42e1fd82796
Aleksbgbg <kozichka01@abv.bg>
2018-08-11 20:56:29 +0100

    [MusicPlayerService] Use TornadoPlayer class to play music.

--- Tornado Player/Services/MusicPlayerService.cs
+++ Tornado Player/Services/MusicPlayerService.cs
@@ -1,38 +1,19 @@
 ﻿namespace Tornado.Player.Services
 {
-    using System;
-    using System.Windows.Media;
-
     using Tornado.Player.Services.Interfaces;
     using Tornado.Player.Utilities;
 
     internal class MusicPlayerService : IMusicPlayerService
     {
-        private readonly MediaPlayer _mediaPlayer = new MediaPlayer();
+        private readonly TornadoPlayer _tornadoPlayer = new TornadoPlayer();
 
         // CTRL + F6 hotkey
         private readonly Win32HotKey _togglePlaybackHotKey = new Win32HotKey(0x75, Win32HotKey.Modifiers.ControlKey | Win32HotKey.Modifiers.NoRepeat);
 
-        private bool _playing = true;
-
-        public MusicPlayerService()
+        public MusicPlayerService(IFileSystemService fileSystemService)
         {
-            _mediaPlayer.Open(new Uri("E:\\MP3s\\Pete & Bas - Shut Ya Mouth [Music Video] Sindhuworld # Grime Report Tv.mp4"));
-            _mediaPlayer.Play();
-
-            _togglePlaybackHotKey.Actuated += (sender, e) =>
-            {
-                if (_playing)
-                {
-                    _mediaPlayer.Pause();
-                }
-                else
-                {
-                    _mediaPlayer.Play();
-                }
-
-                _playing = !_playing;
-            };
+            _tornadoPlayer.Load(fileSystemService.LoadTracks("E:\\MP3s"));
+            _togglePlaybackHotKey.Actuated += (sender, e) => _tornadoPlayer.TogglePlay();
         }
     }
 }
