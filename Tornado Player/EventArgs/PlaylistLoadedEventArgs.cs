f5fa476a22ad2035ad9e169461d68a5bdeb0ce07
Aleksbgbg <kozichka01@abv.bg>
2018-08-12 11:27:04 +0100

    [PlaylistLoadedEventArgs] Add EventArgs for when a new playlist is loaded, providing the new tracks.

---
+++ Tornado Player/EventArgs/PlaylistLoadedEventArgs.cs
@@ -0,0 +1,16 @@
+﻿namespace Tornado.Player.EventArgs
+{
+    using System;
+
+    using Tornado.Player.Models;
+
+    internal class PlaylistLoadedEventArgs : EventArgs
+    {
+        public PlaylistLoadedEventArgs(Track[] tracks)
+        {
+            Tracks = tracks;
+        }
+
+        public Track[] Tracks { get; }
+    }
+}
