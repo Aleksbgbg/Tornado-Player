e8a5e521418a2024ea3637a16f5b55722d9b8d2a
Aleksbgbg <kozichka01@abv.bg>
2018-08-12 11:39:15 +0100

    [PlaylistViewModel] Add ViewModel to contain a collection of tracks currently loaded into the player.

---
+++ Tornado Player/ViewModels/Interfaces/IPlaylistViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Tornado.Player.ViewModels.Interfaces
+{
+    using Caliburn.Micro;
+
+    using Tornado.Player.Models;
+
+    internal interface IPlaylistViewModel : IViewModelBase
+    {
+        IObservableCollection<Track> Tracks { get; }
+    }
+}
---
+++ Tornado Player/ViewModels/PlaylistViewModel.cs
@@ -0,0 +1,28 @@
+﻿namespace Tornado.Player.ViewModels
+{
+    using Caliburn.Micro;
+
+    using Tornado.Player.Models;
+    using Tornado.Player.Services.Interfaces;
+    using Tornado.Player.ViewModels.Interfaces;
+
+    internal class PlaylistViewModel : ViewModelBase, IPlaylistViewModel
+    {
+        private readonly IMusicPlayerService _musicPlayerService;
+
+        public PlaylistViewModel(IMusicPlayerService musicPlayerService)
+        {
+            _musicPlayerService = musicPlayerService;
+
+            Tracks = new BindableCollection<Track>(_musicPlayerService.Tracks);
+
+            _musicPlayerService.PlaylistLoaded += (sender, e) =>
+            {
+                Tracks.Clear();
+                Tracks.AddRange(e.Tracks);
+            };
+        }
+
+        public IObservableCollection<Track> Tracks { get; }
+    }
+}
