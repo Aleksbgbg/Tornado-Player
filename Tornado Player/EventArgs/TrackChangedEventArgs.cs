095e439a01601d47f9b4f9dbc430b3ab21e25b95
Aleksbgbg <kozichka01@abv.bg>
2018-08-12 09:18:17 +0100

    [TrackChangedEventArgs] Create EventArgs class for keeping track of the new track index when switching.

---
+++ Tornado Player/EventArgs/TrackChangedEventArgs.cs
@@ -0,0 +1,14 @@
+﻿namespace Tornado.Player.EventArgs
+{
+    using System;
+
+    internal class TrackChangedEventArgs : EventArgs
+    {
+        public TrackChangedEventArgs(int trackIndex)
+        {
+            TrackIndex = trackIndex;
+        }
+
+        public int TrackIndex { get; }
+    }
+}
