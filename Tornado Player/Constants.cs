a4d36fa3e9574cd075a952d6b5b638df3fe1513c
Aleksbgbg <kozichka01@abv.bg>
2018-08-13 16:47:38 +0100

    [Constants] Add 'Constants' file with global constant fields, such as supported media extensions.

---
+++ Tornado Player/Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace Tornado.Player
+{
+    internal static class Constants
+    {
+        internal static readonly string[] SupportedMediaFormats =
+        {
+                "*.dat", ".3g2", ".3gp", ".3gp2", ".3gpp", ".aac", ".adt", ".adts", ".aif", ".aifc", ".aiff", ".amv", ".asf", ".avi", ".bin", ".cda", ".cue", ".divx", ".dv", ".flv", ".gxf", ".iso", ".m1v", ".m2t", ".m2ts", ".m2v", ".m4a", ".m4v", ".mkv", ".mov", ".mp2", ".mp2v", ".mp3", ".mp4", ".mp4v", ".mpa", ".mpe", ".mpeg", ".mpeg1", ".mpeg2", ".mpeg4", ".mpg", ".mpv2", ".mts", ".nsv", ".nuv", ".ogg", ".ogm", ".ogv", ".ogx", ".ps", ".rec", ".rm", ".rmvb", ".tod", ".ts", ".tts", ".vob", ".vro", ".wav", ".webm", ".wma", ".wmv"
+        };
+    }
+}
