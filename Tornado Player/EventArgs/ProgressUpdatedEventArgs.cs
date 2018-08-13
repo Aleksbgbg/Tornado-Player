ea5801ab7475e73aab1b637174e0cd71239e488f
Aleksbgbg <kozichka01@abv.bg>
2018-08-13 22:11:17 +0100

    [ProgressUpdatedEventArgs] Add EventArgs class for when song progress changes.

---
+++ Tornado Player/EventArgs/ProgressUpdatedEventArgs.cs
@@ -0,0 +1,14 @@
+﻿namespace Tornado.Player.EventArgs
+{
+    using System;
+
+    internal class ProgressUpdatedEventArgs : EventArgs
+    {
+        public ProgressUpdatedEventArgs(TimeSpan newProgress)
+        {
+            NewProgress = newProgress;
+        }
+
+        public TimeSpan NewProgress { get; }
+    }
+}
