096a73107b9d8404677542937280b5fe8ec4feac
Aleksbgbg <kozichka01@abv.bg>
2018-08-13 22:45:23 +0100

    [TimeSpanToSecondsConverter] Convert TimeSpan to TotalSeconds, and vice versa.

---
+++ Tornado Player/Converters/TimeSpanToSecondsConverter.cs
@@ -0,0 +1,22 @@
+﻿namespace Tornado.Player.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    [ValueConversion(typeof(TimeSpan), typeof(double))]
+    internal class TimeSpanToSecondsConverter : IValueConverter
+    {
+        public static TimeSpanToSecondsConverter Default { get; } = new TimeSpanToSecondsConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((TimeSpan)value).TotalSeconds;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return TimeSpan.FromSeconds((double)value);
+        }
+    }
+}
