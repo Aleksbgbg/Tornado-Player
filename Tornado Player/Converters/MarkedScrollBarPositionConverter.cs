9e53fc693f8e79d46df1d0ce364ef536a322d0cb
Aleksbgbg <kozichka01@abv.bg>
2018-08-13 15:28:56 +0100

    [MarkedScrollBarPositionConverter] Create converter to find the correct position of the mark on a marked ScrollBar.

---
+++ Tornado Player/Converters/MarkedScrollBarPositionConverter.cs
@@ -0,0 +1,39 @@
+﻿namespace Tornado.Player.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Controls.Primitives;
+    using System.Windows.Data;
+
+    [ValueConversion(typeof(object[]), typeof(double))]
+    internal class MarkedScrollBarPositionConverter : IMultiValueConverter
+    {
+        public static MarkedScrollBarPositionConverter Default { get; } = new MarkedScrollBarPositionConverter();
+
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            ScrollBar scrollBar = (ScrollBar)values[0];
+            double lineHeight = (double)values[1];
+            int selectedIndex = (int)values[2];
+            int totalItems = (int)values[3];
+
+            double topButtonOffset = ((RepeatButton)scrollBar.Template.FindName("PART_LineUpButton", scrollBar)).ActualHeight;
+            double bottomButtonOffset = ((RepeatButton)scrollBar.Template.FindName("PART_LineDownButton", scrollBar)).ActualHeight;
+
+            double totalScrollSpace = scrollBar.ActualHeight - (topButtonOffset + bottomButtonOffset);
+
+            double linePositionIncrement = totalScrollSpace / totalItems;
+
+            double lineScrollSpacePosition = linePositionIncrement * selectedIndex;
+
+            double absoluteLinePosition = lineScrollSpacePosition + topButtonOffset;
+
+            return absoluteLinePosition - (lineHeight / 2.0);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException($"ConvertBack is not supported on {nameof(MarkedScrollBarPositionConverter)}.");
+        }
+    }
+}
