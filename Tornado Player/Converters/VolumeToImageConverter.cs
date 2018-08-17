55cc4481044d7661c7223f7401be1edc19c651e4
Aleksbgbg <kozichka01@abv.bg>
2018-08-17 13:22:26 +0100

    [VolumeToImageConverter] Add converter to get the right image for the right volume value.

---
+++ Tornado Player/Converters/VolumeToImageConverter.cs
@@ -0,0 +1,41 @@
+﻿namespace Tornado.Player.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows;
+    using System.Windows.Data;
+    using System.Windows.Media;
+
+    [ValueConversion(typeof(double), typeof(ImageSource))]
+    internal class VolumeToImageConverter : IValueConverter
+    {
+        public static VolumeToImageConverter Default { get; } = new VolumeToImageConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double volume = (double)value;
+
+            if (volume == 1.0)
+            {
+                return Application.Current.FindResource("MaxVolume");
+            }
+
+            if (volume >= 0.5)
+            {
+                return Application.Current.FindResource("HighVolume");
+            }
+
+            if (volume > 0.0)
+            {
+                return Application.Current.FindResource("LowVolume");
+            }
+
+            return Application.Current.FindResource("Mute");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException($"ConvertBack is not supported on {nameof(VolumeToImageConverter)}");
+        }
+    }
+}
