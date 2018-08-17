24eb454a8a18793eedfbb9ca838b51c870a1a8ff
Aleksbgbg <kozichka01@abv.bg>
2018-08-17 10:56:18 +0100

    [FractionToPercentageConverter] Add converter which multiplies fractions by 100 to get a percentage, and vice versa.

---
+++ Tornado Player/Converters/FractionToPercentageConverter.cs
@@ -0,0 +1,22 @@
+﻿namespace Tornado.Player.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    [ValueConversion(typeof(double), typeof(double))]
+    internal class FractionToPercentageConverter : IValueConverter
+    {
+        public static FractionToPercentageConverter Default { get; } = new FractionToPercentageConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (double)value * 100.0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (double)value / 100.0;
+        }
+    }
+}
