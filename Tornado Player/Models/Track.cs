c62e4b67684077b29fe36911ea0613c7223aa965
Aleksbgbg <kozichka01@abv.bg>
2018-08-17 18:49:11 +0100

    [Track] Make tracks IComparable to other Tracks.

--- Tornado Player/Models/Track.cs
+++ Tornado Player/Models/Track.cs
@@ -1,8 +1,9 @@
 ﻿namespace Tornado.Player.Models
 {
+    using System;
     using System.IO;
 
-    internal class Track
+    internal class Track : IComparable<Track>
     {
         internal Track(string filepath)
         {
@@ -12,5 +13,10 @@
         public string Filepath { get; }
 
         public string Name => Path.GetFileNameWithoutExtension(Filepath);
+
+        public int CompareTo(Track other)
+        {
+            return string.Compare(Name, other.Name, StringComparison.Ordinal);
+        }
     }
 }
