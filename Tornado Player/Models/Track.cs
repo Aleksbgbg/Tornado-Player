698dbf7088a1541a1f607040c6f9462eec359293
Aleksbgbg <kozichka01@abv.bg>
2018-08-18 19:49:25 +0100

    [Track] Use JSON attributes to decorate class.

--- Tornado Player/Models/Track.cs
+++ Tornado Player/Models/Track.cs
@@ -3,15 +3,20 @@
     using System;
     using System.IO;
 
+    using Newtonsoft.Json;
+
     internal class Track : IComparable<Track>
     {
+        [JsonConstructor]
         internal Track(string filepath)
         {
             Filepath = filepath;
         }
 
+        [JsonProperty("Filepath")]
         public string Filepath { get; }
 
+        [JsonIgnore]
         public string Name => Path.GetFileNameWithoutExtension(Filepath);
 
         public int CompareTo(Track other)
