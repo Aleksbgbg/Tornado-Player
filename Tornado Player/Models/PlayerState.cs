05c619fa122c915c0584826ff2b7946a60f27987
Aleksbgbg <kozichka01@abv.bg>
2018-08-18 19:53:10 +0100

    [PlayerState] Add class for saving state from MusicPlayerService using JSON.

---
+++ Tornado Player/Models/PlayerState.cs
@@ -0,0 +1,34 @@
+﻿namespace Tornado.Player.Models
+{
+    using System;
+
+    using Newtonsoft.Json;
+
+    internal readonly struct PlayerState
+    {
+        [JsonConstructor]
+        internal PlayerState(int trackIndex, TimeSpan progress, double volume, bool loop, bool shuffle)
+        {
+            TrackIndex = trackIndex;
+            Progress = progress;
+            Volume = volume;
+            Loop = loop;
+            Shuffle = shuffle;
+        }
+
+        [JsonProperty("TrackIndex")]
+        internal int TrackIndex { get; }
+
+        [JsonProperty("Progress")]
+        internal TimeSpan Progress { get; }
+
+        [JsonProperty("Volume")]
+        internal double Volume { get; }
+
+        [JsonProperty("Loop")]
+        internal bool Loop { get; }
+
+        [JsonProperty("Shuffle")]
+        internal bool Shuffle { get; }
+    }
+}
