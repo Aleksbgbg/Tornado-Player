0137a5ffa9ee6475541cb64a897b9410d3a87f4d
Aleksbgbg <kozichka01@abv.bg>
2018-08-24 16:01:15 +0100

    [PlaylistView] Scroll selected song into view during Loaded event.

--- Tornado Player/Views/PlaylistView.xaml.cs
+++ Tornado Player/Views/PlaylistView.xaml.cs
@@ -1,10 +1,17 @@
 ﻿namespace Tornado.Player.Views
 {
+    using System.Windows;
+
     public partial class PlaylistView
     {
         public PlaylistView()
         {
             InitializeComponent();
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            Tracks.ScrollIntoView(Tracks.SelectedItem);
         }
     }
 }
