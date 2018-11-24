37ac9b99d039b67b543980f8d470dae0fb4ebff8
Aleksbgbg <kozichka01@abv.bg>
2018-11-24 15:42:57 +0000

    [TrackViewModel] Add method to find track on YouTube, which calls into WebService.

--- Tornado Player/ViewModels/TrackViewModel.cs
+++ Tornado Player/ViewModels/TrackViewModel.cs
@@ -1,15 +1,28 @@
 ﻿namespace Tornado.Player.ViewModels
 {
     using Tornado.Player.Models;
+    using Tornado.Player.Services.Interfaces;
     using Tornado.Player.ViewModels.Interfaces;
 
     internal class TrackViewModel : ViewModelBase, ITrackViewModel
     {
+        private readonly IWebService _webService;
+
+        public TrackViewModel(IWebService webService)
+        {
+            _webService = webService;
+        }
+
         public Track Track { get; private set; }
 
         public void Initialise(Track track)
         {
             Track = track;
+        }
+
+        public void FindOnYouTube()
+        {
+            _webService.YouTubeTrackQueryInBrowser(Track.Name);
         }
     }
 }
