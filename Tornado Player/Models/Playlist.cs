c1b56fc211d6107881a0ddeb5a75bb0ef6ec539f
Aleksbgbg <kozichka01@abv.bg>
2018-11-30 19:18:35 +0000

    [Playlist] Add playlist model.

---
+++ Tornado Player/Models/Playlist.cs
@@ -0,0 +1,26 @@
+﻿namespace Tornado.Player.Models
+{
+    using System.Collections.Generic;
+
+    using Newtonsoft.Json;
+
+    internal class Playlist
+    {
+        [JsonConstructor]
+        internal Playlist(ulong id, string name, IEnumerable<Track> tracks)
+        {
+            Id = id;
+            Name = name;
+            Tracks = new List<Track>(tracks);
+        }
+
+        [JsonProperty("Id")]
+        public ulong Id { get; }
+
+        [JsonProperty("Name")]
+        public string Name { get; }
+
+        [JsonProperty("Tracks")]
+        public List<Track> Tracks { get; }
+    }
+}
