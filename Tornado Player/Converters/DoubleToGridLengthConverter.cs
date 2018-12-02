0dcf92fc66ba25ce82931c16b78f2bd58e3bca3c
Aleksbgbg <kozichka01@abv.bg>
2018-12-02 12:56:32 +0000

    [DoubleToGridLengthConverter] Add converter to convert grid length to absolute pixel value.

---
+++ Tornado Player/Converters/DoubleToGridLengthConverter.cs
@@ -0,0 +1,23 @@
+﻿namespace Tornado.Player.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows;
+    using System.Windows.Data;
+
+    [ValueConversion(typeof(double), typeof(GridLength))]
+    internal class DoubleToGridLengthConverter : IValueConverter
+    {
+        public static DoubleToGridLengthConverter Default { get; } = new DoubleToGridLengthConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return new GridLength((double)value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((GridLength)value).Value;
+        }
+    }
+}
