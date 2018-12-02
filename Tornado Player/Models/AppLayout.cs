7b1ba587b62d1a0e801dd6f52b2ceaa58d4124b1
Aleksbgbg <kozichka01@abv.bg>
2018-12-02 11:47:50 +0000

    [AppLayout] Add model which will store the layout of views in the app. Currently contains a bool indicating whether to show playlists.

---
+++ Tornado Player/Models/AppLayout.cs
@@ -0,0 +1,30 @@
+﻿namespace Tornado.Player.Models
+{
+    using Caliburn.Micro;
+
+    using Newtonsoft.Json;
+
+    internal class AppLayout : PropertyChangedBase
+    {
+        [JsonConstructor]
+        public AppLayout(bool showPlaylists)
+        {
+            _showPlaylists = showPlaylists;
+        }
+
+        private bool _showPlaylists;
+        [JsonProperty("ShowPlaylists")]
+        public bool ShowPlaylists
+        {
+            get => _showPlaylists;
+
+            set
+            {
+                if (_showPlaylists == value) return;
+
+                _showPlaylists = value;
+                NotifyOfPropertyChange(() => ShowPlaylists);
+            }
+        }
+    }
+}
