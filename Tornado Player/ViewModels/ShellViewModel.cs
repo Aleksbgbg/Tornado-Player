07d85d36c5b4137096f65820fc2c67432739e9ba
Aleksbgbg <kozichka01@abv.bg>
2018-12-02 12:25:48 +0000

    [ShellView] Allow users to show or hide playlists through a 'View' menu item.

--- Tornado Player/ViewModels/ShellViewModel.cs
+++ Tornado Player/ViewModels/ShellViewModel.cs
@@ -4,18 +4,22 @@
 
     using Tornado.Player.Helpers;
     using Tornado.Player.Models;
+    using Tornado.Player.Services.Interfaces;
     using Tornado.Player.ViewModels.Interfaces;
 
     internal sealed class ShellViewModel : Conductor<IMainViewModel>, IShellViewModel
     {
-        public ShellViewModel(IEventAggregator eventAggregator, IMainViewModel mainViewModel)
+        public ShellViewModel(IEventAggregator eventAggregator, ILayoutService layoutService, IMainViewModel mainViewModel)
         {
             DisplayName = "Tornado Player";
 
+            AppLayout = layoutService.AppLayout;
             SearchCommand = new RelayCommand(_ => eventAggregator.BeginPublishOnUIThread(Shortcut.Search));
 
             ActivateItem(mainViewModel);
         }
+
+        public AppLayout AppLayout { get; }
 
         public RelayCommand SearchCommand { get; }
     }
