88d90071140f98e9d1d5b6c3b1e1037d1dd6e7fd
Aleksbgbg <kozichka01@abv.bg>
2018-12-15 12:30:40 +0000

    [ResourceConverter] Add converter which finds app resources.

---
+++ Tornado Player/Converters/ResourceConverter.cs
@@ -0,0 +1,23 @@
+﻿namespace Tornado.Player.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows;
+    using System.Windows.Data;
+
+    [ValueConversion(typeof(string), typeof(object))]
+    internal class ResourceConverter : IValueConverter
+    {
+        public static ResourceConverter Default { get; } = new ResourceConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Application.Current.TryFindResource(value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException($"ConvertBack is not supported {nameof(ResourceConverter)}.");
+        }
+    }
+}
