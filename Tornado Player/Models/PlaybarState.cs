d583836033780435268330c3fc6d01abc84c8079
Aleksbgbg <kozichka01@abv.bg>
2018-12-17 21:47:39 +0000

    [PlaybarState] Add model to store the playbar state (volume and loop).

---
+++ Tornado Player/Models/PlaybarState.cs
@@ -0,0 +1,20 @@
+﻿namespace Tornado.Player.Models
+{
+    using Newtonsoft.Json;
+
+    internal class PlaybarState
+    {
+        [JsonConstructor]
+        internal PlaybarState(double volume, bool loop)
+        {
+            Volume = volume;
+            Loop = loop;
+        }
+
+        [JsonProperty("Volume")]
+        internal double Volume { get; }
+
+        [JsonProperty("Loop")]
+        internal bool Loop { get; }
+    }
+}
