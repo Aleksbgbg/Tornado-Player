f96b1387995a1a9cc2886ee0e4b5038e6f8ac145
Aleksbgbg <kozichka01@abv.bg>
2018-12-18 21:35:46 +0000

    [DialogViewModel] Add base DialogViewModel which stores model data.

---
+++ Tornado Player/ViewModels/Dialogs/DialogViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Tornado.Player.ViewModels.Dialogs
+{
+    using Tornado.Player.ViewModels.Interfaces.Dialogs;
+
+    internal abstract class DialogViewModel<TModel> : ViewModelBase, IDialogViewModel<TModel>
+    {
+        private protected DialogViewModel(TModel data)
+        {
+            Data = data;
+        }
+
+        public TModel Data { get; }
+    }
+}
---
+++ Tornado Player/ViewModels/Interfaces/Dialogs/IDialogViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Tornado.Player.ViewModels.Interfaces.Dialogs
+{
+    internal interface IDialogViewModel<out TModel>
+    {
+        TModel Data { get; }
+    }
+}
