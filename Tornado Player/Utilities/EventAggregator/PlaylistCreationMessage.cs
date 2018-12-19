651f4f25d390293a617923d50941234e249632ca
Aleksbgbg <kozichka01@abv.bg>
2018-12-19 21:50:55 +0000

    [PlaylistCreationMessage] Add EventAggregator message containing an IPlaylistViewModel, sent on creation.

---
+++ Tornado Player/Utilities/EventAggregator/PlaylistCreationMessage.cs
@@ -0,0 +1,14 @@
+﻿namespace Tornado.Player.Utilities.EventAggregator
+{
+    using Tornado.Player.ViewModels.Interfaces;
+
+    internal class PlaylistCreationMessage
+    {
+        public PlaylistCreationMessage(IPlaylistViewModel playlistViewModel)
+        {
+            PlaylistViewModel = playlistViewModel;
+        }
+
+        public IPlaylistViewModel PlaylistViewModel { get; }
+    }
+}
