5e2e7368b8e135692b7933c45e814c272dbd63ae
Aleksbgbg <kozichka01@abv.bg>
2018-12-22 15:04:19 +0000

    [FocusOnLoadedBehavior] Add behavior to focus TextBoxes when they load.

---
+++ Tornado Player/Helpers/FocusOnLoadedBehavior.cs
@@ -0,0 +1,13 @@
+﻿namespace Tornado.Player.Helpers
+{
+    using System.Windows.Controls;
+    using System.Windows.Interactivity;
+
+    internal class FocusOnLoadedBehavior : Behavior<TextBox>
+    {
+        protected override void OnAttached()
+        {
+            AssociatedObject.Loaded += (sender, e) => AssociatedObject.Focus();
+        }
+    }
+}
