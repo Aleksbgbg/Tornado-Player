64844d1437134b6e93a9d180e08b0f8c1808f9be
Aleksbgbg <kozichka01@abv.bg>
2018-12-22 13:48:04 +0000

    [DialogService] Add strongly-typed settings parameter and serialize into dictionary to WindowManager.

--- Tornado Player/Services/DialogService.cs
+++ Tornado Player/Services/DialogService.cs
@@ -1,8 +1,12 @@
 ﻿namespace Tornado.Player.Services
 {
+    using System.Collections.Generic;
+    using System.Linq;
+
     using Caliburn.Micro;
     using Caliburn.Micro.Wrapper;
 
+    using Tornado.Player.Models;
     using Tornado.Player.Services.Interfaces;
     using Tornado.Player.ViewModels.Interfaces.Dialogs;
 
@@ -18,19 +22,36 @@
             _viewModelFactory = viewModelFactory;
         }
 
-        public TModel ShowDialog<TModel, TViewModel>()
+        public TModel ShowDialog<TModel, TViewModel>(WindowSettings windowSettings = default)
                 where TModel : new()
                 where TViewModel : IDialogViewModel<TModel>
         {
-            return ShowDialog<TModel, TViewModel>(new TModel());
+            return ShowDialog<TModel, TViewModel>(new TModel(), windowSettings);
         }
 
-        public TModel ShowDialog<TModel, TViewModel>(TModel model)
+        public TModel ShowDialog<TModel, TViewModel>(TModel model, WindowSettings windowSettings = default)
                 where TViewModel : IDialogViewModel<TModel>
         {
             TViewModel viewModel = _viewModelFactory.MakeViewModel<TViewModel>(model);
 
-            _windowManager.ShowDialog(viewModel);
+            if (windowSettings == null)
+            {
+                _windowManager.ShowDialog(viewModel);
+            }
+            else
+            {
+                Dictionary<string, object> settings = typeof(WindowSettings).GetProperties()
+                                                                            .Select(property => new
+                                                                            {
+                                                                                Name = property.Name,
+                                                                                Value = property.GetValue(windowSettings)
+                                                                            })
+                                                                            .Where(property => property.Value != null)
+                                                                            .ToDictionary(property => property.Name,
+                                                                                          property => property.Value);
+
+                _windowManager.ShowDialog(viewModel, settings: settings);
+            }
 
             return viewModel.Data;
         }
--- Tornado Player/Services/Interfaces/IDialogService.cs
+++ Tornado Player/Services/Interfaces/IDialogService.cs
@@ -1,12 +1,15 @@
 ﻿namespace Tornado.Player.Services.Interfaces
 {
+    using Tornado.Player.Models;
     using Tornado.Player.ViewModels.Interfaces.Dialogs;
 
     internal interface IDialogService
     {
-        TModel ShowDialog<TModel, TViewModel>() where TModel : new()
-                                                where TViewModel : IDialogViewModel<TModel>;
+        TModel ShowDialog<TModel, TViewModel>(WindowSettings windowSettings = default)
+                where TModel : new()
+                where TViewModel : IDialogViewModel<TModel>;
 
-        TModel ShowDialog<TModel, TViewModel>(TModel model) where TViewModel : IDialogViewModel<TModel>;
+        TModel ShowDialog<TModel, TViewModel>(TModel model, WindowSettings windowSettings = default)
+                where TViewModel : IDialogViewModel<TModel>;
     }
 }
