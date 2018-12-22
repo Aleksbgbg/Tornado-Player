4ffea5ef6180c789a7d0d3642ef9aa7f15b1c877
Aleksbgbg <kozichka01@abv.bg>
2018-12-22 13:47:13 +0000

    [WindowSettings] Add model to store data about the way in which a window is displayed.

---
+++ Tornado Player/Models/WindowSettings.cs
@@ -0,0 +1,25 @@
+﻿namespace Tornado.Player.Models
+{
+    using System.Windows;
+
+    public class WindowSettings
+    {
+        public ResizeMode ResizeMode { get; set; } = ResizeMode.CanResize;
+
+        public bool ShowInTaskbar { get; set; } = true;
+
+        public string Title { get; set; }
+
+        public bool Topmost { get; set; } = false;
+
+        public double Height { get; set; }
+
+        public double Width { get; set; }
+
+        public WindowStartupLocation WindowStartupLocation { get; set; } = WindowStartupLocation.CenterOwner;
+
+        public WindowState WindowState { get; set; } = WindowState.Normal;
+
+        public WindowStyle WindowStyle { get; set; } = WindowStyle.SingleBorderWindow;
+    }
+}
