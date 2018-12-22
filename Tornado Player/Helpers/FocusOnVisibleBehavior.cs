18f4fe666887630338790c6c3ef42d671041354b
Aleksbgbg <kozichka01@abv.bg>
2018-12-22 15:10:05 +0000

    [FocusOnVisibleBehavior] Add behavior to focus a TextBox when it becomes visible.

---
+++ Tornado Player/Helpers/FocusOnVisibleBehavior.cs
@@ -0,0 +1,19 @@
+﻿namespace Tornado.Player.Helpers
+{
+    using System.Windows.Controls;
+    using System.Windows.Interactivity;
+
+    internal class FocusOnVisibleBehavior : Behavior<TextBox>
+    {
+        protected override void OnAttached()
+        {
+            AssociatedObject.IsVisibleChanged += (sender, e) =>
+            {
+                if ((bool)e.NewValue)
+                {
+                    AssociatedObject.Focus();
+                }
+            };
+        }
+    }
+}
