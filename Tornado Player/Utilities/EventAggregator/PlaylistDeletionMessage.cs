1d1bf2f0af9f53be5b5547ba51c612cf331cc246
Aleksbgbg <kozichka01@abv.bg>
2018-12-22 19:22:48 +0000

    [PlaylistDeletionMessage] Add message singifying the deletion of a playlist.

---
+++ Tornado Player/Utilities/EventAggregator/PlaylistDeletionMessage.cs
@@ -0,0 +1,14 @@
+﻿namespace Tornado.Player.Utilities.EventAggregator
+{
+    using Tornado.Player.ViewModels.Interfaces;
+
+    internal class PlaylistDeletionMessage
+    {
+        internal PlaylistDeletionMessage(IPlaylistViewModel playlistViewModel)
+        {
+            PlaylistViewModel = playlistViewModel;
+        }
+
+        public IPlaylistViewModel PlaylistViewModel { get; }
+    }
+}
