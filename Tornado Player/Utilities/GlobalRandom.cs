03680088546dfd6b312ae69f06399db913298deb
Aleksbgbg <kozichka01@abv.bg>
2019-01-04 12:47:20 +0000

    [GlobalRandom] Add utility which delegates calls to Random class for global use.

---
+++ Tornado Player/Utilities/GlobalRandom.cs
@@ -0,0 +1,34 @@
+﻿namespace Tornado.Player.Utilities
+{
+    using System;
+
+    internal static class GlobalRandom
+    {
+        private static readonly Random Random = new Random();
+
+        internal static int Next()
+        {
+            return Random.Next();
+        }
+
+        internal static int Next(int maxValue)
+        {
+            return Random.Next(maxValue);
+        }
+
+        internal static int Next(int minValue, int maxValue)
+        {
+            return Random.Next(minValue, maxValue);
+        }
+
+        internal static double NextDouble()
+        {
+            return Random.NextDouble();
+        }
+
+        internal static void NextBytes(byte[] buffer)
+        {
+            Random.NextBytes(buffer);
+        }
+    }
+}
