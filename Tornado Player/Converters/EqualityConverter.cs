3b6e5b3ef797967e8604245b13575361ca4fde67
Aleksbgbg <kozichka01@abv.bg>
2019-02-20 15:31:38 +0000

    [Converters] Add equality and not operator converters.

---
+++ Tornado Player/Converters/EqualityConverter.cs
@@ -0,0 +1,23 @@
+﻿namespace Tornado.Player.Converters
+{
+    using System;
+    using System.ComponentModel;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    [ValueConversion(typeof(object), typeof(bool))]
+    internal class EqualityConverter : IValueConverter
+    {
+        public static EqualityConverter Default { get; } = new EqualityConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Equals(value, parameter);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException($"ConvertBack is not supported on {nameof(EqualityConverter)}");
+        }
+    }
+}
---
+++ Tornado Player/Converters/NotConverter.cs
@@ -0,0 +1,22 @@
+﻿namespace Tornado.Player.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    [ValueConversion(typeof(bool), typeof(bool))]
+    internal class NotConverter : IValueConverter
+    {
+        public static NotConverter Default { get; } = new NotConverter();
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !((bool)value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !((bool)value);
+        }
+    }
+}
