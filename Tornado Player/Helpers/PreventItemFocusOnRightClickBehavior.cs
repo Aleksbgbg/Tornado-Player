e13fedc9ff49c5a12d1f8e99aa3c40319fd12888
Aleksbgbg <kozichka01@abv.bg>
2019-02-20 13:07:03 +0000

    [PlaylistView] Prevent item focus on right click (to allow context menu on non-selected items).

---
+++ Tornado Player/Helpers/PreventItemFocusOnRightClickBehavior.cs
@@ -0,0 +1,13 @@
+﻿namespace Tornado.Player.Helpers
+{
+    using System.Windows.Controls;
+    using System.Windows.Interactivity;
+
+    internal class PreventItemFocusOnRightClickBehavior : Behavior<ListBox>
+    {
+        protected override void OnAttached()
+        {
+            AssociatedObject.PreviewMouseRightButtonDown += (sender, e) => e.Handled = true;
+        }
+    }
+}
