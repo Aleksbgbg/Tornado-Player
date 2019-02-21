5313d59664b52af37e0a2be555a9801d52ce9545
Aleksbgbg <kozichka01@abv.bg>
2019-02-21 12:57:50 +0000

    [Settings] Use declaration attributes to describe HotKeyBinds array for the settings editor.

--- Tornado Player/Models/Settings/Settings.cs
+++ Tornado Player/Models/Settings/Settings.cs
@@ -1,16 +1,24 @@
 ﻿namespace Tornado.Player.Models.Settings
 {
+    using System.ComponentModel;
+
     using Newtonsoft.Json;
+
+    using Xceed.Wpf.Toolkit.PropertyGrid.Attributes;
 
     internal class Settings
     {
         [JsonConstructor]
         internal Settings(HotKeyBind[] hotKeyBinds)
         {
-            HotKeyBinds = hotKeyBinds;
+            HotKeyBinds = new HotKeyBindArray(hotKeyBinds);
         }
 
+        [DisplayName("Hotkey Binds")]
+        [Category("Hotkeys")]
+        [Description("All of the global (system-wide) hotkeys for various player actions.")]
+        [ExpandableObject]
         [JsonProperty(nameof(HotKeyBinds))]
-        public HotKeyBind[] HotKeyBinds { get; }
+        public HotKeyBindArray HotKeyBinds { get; }
     }
 }
