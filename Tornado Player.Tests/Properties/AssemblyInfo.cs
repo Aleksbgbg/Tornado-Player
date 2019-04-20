a52fa97b575a85e503a55785dcdd1ecca431632d
Aleksbgbg <kozichka01@abv.bg>
2019-04-20 18:29:17 +0100

    [Tornado Player.Tests] Add unit test project.

---
+++ Tornado Player.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tornado Player.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tornado Player.Tests")]
+[assembly: AssemblyCopyright("Copyright © Aleks Todorov 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("7493ba8c-e702-4728-a21c-0b66a4691850")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
