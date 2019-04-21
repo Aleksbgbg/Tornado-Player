6ce4f58de785714e41383ced9c8e6d0b106fc864
Aleksbgbg <kozichka01@abv.bg>
2019-04-21 17:16:16 +0100

    [TrackFoldersViewModel] Add method to add new track folder.

--- Tornado Player.Tests/ViewModels/TrackFoldersViewModelTests.cs
+++ Tornado Player.Tests/ViewModels/TrackFoldersViewModelTests.cs
@@ -16,6 +16,8 @@
     {
         private readonly Mock<IStorageService> _storageServiceMock;
 
+        private FileSystemBrowserServiceMock _fileSystemBrowserServiceMock;
+
         private TrackFoldersViewModel _trackFoldersViewModel;
 
         public TrackFoldersViewModelTests()
@@ -42,15 +44,108 @@
             Assert.Equal(folders, _trackFoldersViewModel.Folders);
         }
 
+        [Fact]
+        public void TestAddNewFolderCreatesDialog()
+        {
+            SetupDirectorySelected(null);
+
+            _trackFoldersViewModel.AddNewFolder();
+
+            VerifyBrowseFolderCalled();
+        }
+
+        [Fact]
+        public void TestAddNewFolderAddsFolderToCollection()
+        {
+            const string directory = "SomeSongDirectory";
+            SetupDirectorySelected(directory);
+
+            _trackFoldersViewModel.AddNewFolder();
+
+            Assert.Collection(_trackFoldersViewModel.Folders, trackFolder => Assert.Equal(directory, trackFolder.Path));
+        }
+
+        [Fact]
+        public void TestAddNewFolderStoresNewFolder()
+        {
+            const string directory = "SomeSongDirectory";
+            SetupDirectorySelected(directory);
+
+            _trackFoldersViewModel.AddNewFolder();
+
+            Assert.Collection(_storageServiceMock.Object.TrackFolders, trackFolder => Assert.Equal(directory, trackFolder.Path));
+        }
+
+        [Fact]
+        public void TestAddNewFolderDoesNotAddFolderToCollectionWhenNotSelected()
+        {
+            SetupDirectoryNotSelected();
+
+            _trackFoldersViewModel.AddNewFolder();
+
+            Assert.Empty(_trackFoldersViewModel.Folders);
+        }
+
+        [Fact]
+        public void TestAddNewFolderDoesNotStoreNewFolderWhenNotSelected()
+        {
+            SetupDirectoryNotSelected();
+
+            _trackFoldersViewModel.AddNewFolder();
+
+            Assert.Empty(_storageServiceMock.Object.TrackFolders);
+        }
+
+        private void SetupDirectoryNotSelected()
+        {
+            SetupStorageServiceFolders(new List<TrackFolder>());
+            _fileSystemBrowserServiceMock = new FileSystemBrowserServiceMock(false, null);
+            CreateInstance();
+        }
+
+        private void SetupDirectorySelected(string directory)
+        {
+            SetupStorageServiceFolders(new List<TrackFolder>());
+            _fileSystemBrowserServiceMock = new FileSystemBrowserServiceMock(true, directory);
+            CreateInstance();
+        }
+
         private void CreateInstance()
         {
-            _trackFoldersViewModel = new TrackFoldersViewModel(_storageServiceMock.Object);
+            _trackFoldersViewModel = new TrackFoldersViewModel(_storageServiceMock.Object, _fileSystemBrowserServiceMock);
         }
 
         private void SetupStorageServiceFolders(List<TrackFolder> folders)
         {
             _storageServiceMock.SetupGet(storageService => storageService.TrackFolders)
                                .Returns(folders);
+        }
+
+        private void VerifyBrowseFolderCalled()
+        {
+            Assert.Equal(1, _fileSystemBrowserServiceMock.Calls);
+        }
+
+        private class FileSystemBrowserServiceMock : IFileSystemBrowserService
+        {
+            private readonly bool _exists;
+
+            private readonly string _directory;
+
+            public FileSystemBrowserServiceMock(bool exists, string directory)
+            {
+                _exists = exists;
+                _directory = directory;
+            }
+
+            public int Calls { get; private set; }
+
+            public bool BrowseDirectory(out string directory)
+            {
+                ++Calls;
+                directory = _directory;
+                return _exists;
+            }
         }
     }
 }
--- Tornado Player/ViewModels/TrackFoldersViewModel.cs
+++ Tornado Player/ViewModels/TrackFoldersViewModel.cs
@@ -8,11 +8,31 @@
 
     public class TrackFoldersViewModel : ViewModelBase, ITrackFoldersViewModel
     {
-        public TrackFoldersViewModel(IStorageService storageService)
+        private readonly IStorageService _storageService;
+
+        private readonly IFileSystemBrowserService _fileSystemBrowserService;
+
+        public TrackFoldersViewModel(IStorageService storageService, IFileSystemBrowserService fileSystemBrowserService)
         {
+            _storageService = storageService;
+            _fileSystemBrowserService = fileSystemBrowserService;
+
             Folders = new ObservableCollection<TrackFolder>(storageService.TrackFolders);
         }
 
         public ObservableCollection<TrackFolder> Folders { get; }
+
+        public void AddNewFolder()
+        {
+            bool directorySelected = _fileSystemBrowserService.BrowseDirectory(out string directory);
+
+            if (directorySelected)
+            {
+                TrackFolder newTrackFolder = new TrackFolder(directory);
+
+                _storageService.TrackFolders.Add(newTrackFolder);
+                Folders.Add(newTrackFolder);
+            }
+        }
     }
 }
